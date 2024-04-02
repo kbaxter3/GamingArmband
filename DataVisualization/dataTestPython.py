1b33ca0cc811c4bef281e4dedac40493200248b7
wazo10 <86400160+wazo10@users.noreply.github.com>
2024-04-02 16:50:05 -0500

    Implement barebones EMG graph

--- DataVisualization/dataTestPython.py
+++ DataVisualization/dataTestPython.py
@@ -1,122 +1,204 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 29 22:06:03 2024
+# Created on Thu Feb 29 22:06:03 2024
 
-@author: Katerina
-"""
+# @author: Katerina
 
-# Importing Libraries 
-import serial 
-import time 
+import os
+# Importing Libraries
+# import serial
+
+import time
 import copy
+
+import matplotlib.pyplot as plt
 import numpy as np
 import random
-import matplotlib.pyplot as plt
+import pygame
+from pygame.locals import *
+from collections import deque
+
+f = open("/dev/null",  "w")
+os.dup2(f.fileno(), 2)
+f.close()
+
+screen_width = 1600
+screen_height = 900
+
+# Define global variables
+EMG_DATA_SIZE = screen_width - 100  # Assuming 100 samples per second for x seconds
+emg_data_buffer = deque(maxlen=EMG_DATA_SIZE)
+x_scale_factor = EMG_DATA_SIZE / 100  # Adjust the scale factor as needed
 
 # stores newest data
 class DataSample:
-    emg = 0;
-    xOrientation = 0;
-    yOrientation = 0;
-    zOrientation = 0;
-    xGyro = 0;
-    yGyro = 0;
-    zGyro = 0;
-    xLinAccel = 0;
-    yLinAccel = 0;
-    zLinAccel = 0;
-    
+    emg = 0
+    xOrientation = 0
+    yOrientation = 0
+    zOrientation = 0
+    xGyro = 0
+    yGyro = 0
+    zGyro = 0
+    xLinAccel = 0
+    yLinAccel = 0
+    zLinAccel = 0
+
+
 # @William job
 def initGraphics():
-    #do smth
-    return 0
-    
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Live Data Visualization")
+    pygame.display.flip()
+    return screen, True
+
+
 # @William job
-def updateGraphics(dataPoint):
-    # do smth
-    return 0
+def updateGraphics(screen, dataPoint):
+    if screen is None:
+        return
+
+    emg_data_buffer.append(dataPoint.emg)
+    # Clear the screen
+    screen.fill((19, 41, 75)) # uiuc orange background
+    # (255, 95, 5) is uiuc orange
+    # (19, 41, 75) is uiuc blue
+    # pygame.draw.rect(screen, (19, 41, 75), (10, 10, screen_width / 2, screen_height / 2)) # top left
+    # pygame.draw.rect(screen, (19, 41, 75), (screen_width / 2 + 20, screen_height / 2 + 20, screen_width / 2 - 30, screen_height / 2 - 30)) # bottom right
+    # pygame.draw.rect(screen, (19, 41, 75), (10, screen_height / 2 + 20, screen_width / 2, screen_height / 2 - 30)) # bottom left
+    # pygame.draw.rect(screen, (19, 41, 75), (screen_width / 2 + 20, 10, screen_width / 2 - 30, screen_height / 2)) # top right
+
+    draw_emg_graph(screen)
+    # Update the display
+    pygame.display.flip()
+
+
+def draw_emg_graph(screen):
+    global emg_data_buffer
+
+    x_margin = 10
+    y_margin = 10
+
+    graph_top_left_x = x_margin
+    graph_top_left_y = y_margin
+    graph_width = screen_width - x_margin
+    graph_height = screen_height - y_margin
+
+    pygame.draw.line(screen, (255, 255, 255), (graph_top_left_x, graph_top_left_y + graph_height / 2),
+                     (graph_top_left_x + graph_width - x_margin, graph_top_left_y + graph_height / 2), 2)  # X-axis
+    pygame.draw.line(screen, (255, 255, 255), (graph_top_left_x, graph_top_left_y),
+                     (graph_top_left_x, graph_top_left_y + graph_height - y_margin), 2)  # Y-axis
+
+    font = pygame.font.SysFont('Arial', 20)
+
+    label_y = font.render('EMG', True, (255, 255, 255))
+    label_x = font.render('Time', True, (255, 255, 255))
+    screen.blit(label_y, (graph_top_left_x + 10, graph_top_left_y))
+    screen.blit(label_x, (graph_top_left_x + graph_width - 50, graph_top_left_y + graph_height / 2 + 5))
+
+    emg_points = list(emg_data_buffer)
+    max_emg_value = max(emg_points) if emg_points else 0
+    min_emg_value = min(emg_points) if emg_points else 0
+
+    max_val_label = font.render(f'{max_emg_value:.2f}', True, (255, 255, 255))
+    min_val_label = font.render(f'{min_emg_value:.2f}', True, (255, 255, 255))
+
+    screen.blit(max_val_label, (graph_top_left_x + 5, graph_top_left_y + 30))
+    screen.blit(min_val_label, (graph_top_left_x + 5, graph_top_left_y + graph_height - 50))
+
+    pygame.draw.line(screen, (255, 255, 255), (graph_top_left_x - 5, graph_top_left_y + 50), (graph_top_left_x + 5, graph_top_left_y + 50), 2)  # Max value tick
+    pygame.draw.line(screen, (255, 255, 255), (graph_top_left_x - 5, graph_top_left_y + graph_height - 50), (graph_top_left_x + 5, graph_top_left_y + graph_height - 50), 2)  # Min value tick
+
+    emg_points = list(emg_data_buffer)
+    if len(emg_points) < 2:
+        return
+
+    # Scale the EMG data to fit the screen
+    max_emg_value = max(emg_points) if max(emg_points) > 0 else 1
+    scaled_emg_points = [int(((p / max_emg_value) * (graph_height - 100)) + graph_top_left_y + 50) for p in emg_points]
+
+    # Draw lines connecting consecutive EMG data points
+    for i in range(1, len(scaled_emg_points)):
+        pygame.draw.line(screen, (255, 255, 255), (i - 1 + graph_top_left_x, scaled_emg_points[i - 1]), (i + graph_top_left_x, scaled_emg_points[i]), 1)
+
 
 # Reads all new points from Serial Port
 # Returns DataSample class
 def readData(serial):
     newData = DataSample()
     serialMsg = serial.readline()
-    while(serialMsg == b''):
+    while serialMsg == b'':
         serialMsg = serial.readline()
-    
+
     newDataValues = serialMsg.decode('ascii').split(',')
-    
-    newData.emg            = int(newDataValues[0].split(':')[1]) / 4096
-    newData.xOrientation   = float(newDataValues[1].split(':')[1])
-    newData.yOrientation   = float(newDataValues[2].split(':')[1])
-    newData.zOrientation   = float(newDataValues[3].split(':')[1])
-    newData.xGyro          = float(newDataValues[4].split(':')[1])
-    newData.yGyro          = float(newDataValues[5].split(':')[1])
-    newData.zGyro          = float(newDataValues[6].split(':')[1])
-    newData.xLinAccel      = float(newDataValues[7].split(':')[1])
-    newData.yLinAccel      = float(newDataValues[8].split(':')[1])
-    newData.zLinAccel      = float(newDataValues[9].split(':')[1])
-    
+
+    newData.emg = int(newDataValues[0].split(':')[1]) / 4096
+    newData.xOrientation = float(newDataValues[1].split(':')[1])
+    newData.yOrientation = float(newDataValues[2].split(':')[1])
+    newData.zOrientation = float(newDataValues[3].split(':')[1])
+    newData.xGyro = float(newDataValues[4].split(':')[1])
+    newData.yGyro = float(newDataValues[5].split(':')[1])
+    newData.zGyro = float(newDataValues[6].split(':')[1])
+    newData.xLinAccel = float(newDataValues[7].split(':')[1])
+    newData.yLinAccel = float(newDataValues[8].split(':')[1])
+    newData.zLinAccel = float(newDataValues[9].split(':')[1])
+
     return newData
 
-# Use for testing 
+
+# Use for testing
 # randomly updates all values to simulate new readings
 def testReadData(oldData):
     newData = copy.deepcopy(oldData)
-    newData.emg            = newData.emg + random.random() - 0.5
-    newData.xOrientation   = (newData.xOrientation + (random.random()-0.5) * 15) 
-    newData.yOrientation   = (newData.yOrientation + (random.random()-0.5) * 15) 
-    newData.zOrientation   = (newData.zOrientation + (random.random()-0.5) * 15)
-    newData.xGyro          = (random.random()-0.5) * 10
-    newData.yGyro          = (random.random()-0.5) * 10
-    newData.zGyro          = (random.random()-0.5) * 10
-    newData.xLinAccel      = (random.random()-0.5) * 10
-    newData.yLinAccel      = (random.random()-0.5) * 10
-    newData.zLinAccel      = (random.random()-0.5) * 10
-    
-    
-    if(newData.emg < 0): 
+    newData.emg = newData.emg + random.random() - 0.5
+    newData.xOrientation = (newData.xOrientation + (random.random() - 0.5) * 15)
+    newData.yOrientation = (newData.yOrientation + (random.random() - 0.5) * 15)
+    newData.zOrientation = (newData.zOrientation + (random.random() - 0.5) * 15)
+    newData.xGyro = (random.random() - 0.5) * 10
+    newData.yGyro = (random.random() - 0.5) * 10
+    newData.zGyro = (random.random() - 0.5) * 10
+    newData.xLinAccel = (random.random() - 0.5) * 10
+    newData.yLinAccel = (random.random() - 0.5) * 10
+    newData.zLinAccel = (random.random() - 0.5) * 10
+
+    if newData.emg < 0:
         newData.emg = 0
-    if(newData.emg > 1):
+    if newData.emg > 1:
         newData.emg = 1
-    if(newData.xOrientation > 360):
+    if newData.xOrientation > 360:
         newData.xOrientation -= 360
-    if(newData.xOrientation < 0):
+    if newData.xOrientation < 0:
         newData.xOrientation += 360
-    if(newData.yOrientation > 360):
+    if newData.yOrientation > 360:
         newData.yOrientation -= 360
-    if(newData.yOrientation < 0):
+    if newData.yOrientation < 0:
         newData.yOrientation += 360
-    if(newData.zOrientation > 360):
+    if newData.zOrientation > 360:
         newData.zOrientation -= 360
-    if(newData.zOrientation < 0):
+    if newData.zOrientation < 0:
         newData.zOrientation += 360
-    
+
     return newData
-    
+
 
 # set up serial
 # commented out for testing
 # arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1) 
 
-initGraphics()
 
-data = DataSample()
+screen, window_created = initGraphics()
+if not window_created:
+    print("Failed to create Pygame window")
+    exit()
 
 # Main loop
-while(True): 
-    
+running = True
+data = DataSample()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
     data = testReadData(data)
-    
-    updateGraphics(data)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-	
-	
+    updateGraphics(screen, data)
+
+pygame.quit()
